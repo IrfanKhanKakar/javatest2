e85e6fd7761f6e5eac10e1bf74d0cd47ad1355db
IRFANKHAN <irfankakar215@gmail.com>
2017-04-11 22:23:25 -0700

    transper message to other frame

---
+++ TestProject/src/FrameOne.java
@@ -0,0 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrameOne extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldName;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameOne frame = new FrameOne();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameOne() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textFieldName = new JTextField();
+		textFieldName.setBounds(150, 65, 86, 20);
+		contentPane.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String name = textFieldName.getText();
+				
+				FrameTwo frameTwo = new FrameTwo(name);
+				frameTwo.setVisible(true);
+			
+			}
+		});
+		btnSend.setBounds(231, 161, 89, 23);
+		contentPane.add(btnSend);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(64, 68, 46, 14);
+		contentPane.add(lblName);
+	}
+}
---
+++ TestProject/src/FrameTwo.java
@@ -0,0 +1,73 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class FrameTwo extends JFrame {
+
+	private JPanel contentPane;
+	private String Name;
+	JLabel Recieved;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameTwo frame = new FrameTwo("");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameTwo(String name)
+	{
+		Name = name;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMessage = new JLabel("Message");
+		lblMessage.setBounds(33, 124, 105, 14);
+		contentPane.add(lblMessage);
+		
+		Recieved = new JLabel("New label");
+		Recieved.setBounds(159, 124, 207, 14);
+		contentPane.add(Recieved);
+		
+		
+		Recieved.setText(Name);
+	}
+	
+	/*public void jaman() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMessage = new JLabel("Message");
+		lblMessage.setBounds(33, 124, 46, 14);
+		contentPane.add(lblMessage);
+		
+		Recieved = new JLabel("New label");
+		Recieved.setBounds(159, 124, 46, 14);
+		contentPane.add(Recieved);
+	}*/
+
+}
